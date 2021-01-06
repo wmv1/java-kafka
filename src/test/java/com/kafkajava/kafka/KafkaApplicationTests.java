bcd0dc0f36e9eee9e93e5b51125e0d4f308a223f
Willian Vieira <willianmarquesv@gmail.com>
2021-01-06 17:21:57 -0300

    First commit

---
+++ src/test/java/com/kafkajava/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkajava.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
